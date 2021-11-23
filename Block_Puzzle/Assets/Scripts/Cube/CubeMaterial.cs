01feaa18f4941655eeac548dbb3fce1732567b1e
sa04108 <sa04108@naver.com>
2021-11-24 02:23:29 +0900

    4x4x4 큐브 See Through 기능 추가

--- Block_Puzzle/Assets/Scripts/Cube/CubeMaterial.cs
+++ Block_Puzzle/Assets/Scripts/Cube/CubeMaterial.cs
@@ -10,10 +10,12 @@ public class CubeMaterial : MonoBehaviour
 
     protected bool cube222;
     protected bool cube333;
+    protected bool cube444;
 
     protected GameObject[] firstFloor;
     protected GameObject[] secondFloor;
     protected GameObject[] thirdFloor;
+    protected GameObject[] fourthFloor;
 
     Button seeThroughButton;
 
@@ -24,10 +26,14 @@ public class CubeMaterial : MonoBehaviour
 
         InitFloors();
 
+        seeThroughButton = GameObject.Find("See Through Button").GetComponent<Button>();
         if (cube333)
         {
-            seeThroughButton = GameObject.Find("See Through Button").GetComponent<Button>();
             seeThroughButton.onClick.AddListener(Set333CubeAlpha);
+        }
+        else if (cube444)
+        {
+            seeThroughButton.onClick.AddListener(Set444CubeAlpha);
         }
     }
 
@@ -37,12 +43,18 @@ public class CubeMaterial : MonoBehaviour
 
         cube222 = cubeBlocks.floors.Count == 2;
         cube333 = cubeBlocks.floors.Count == 3;
+        cube444 = cubeBlocks.floors.Count == 4;
 
         firstFloor = cubeBlocks.floors[0].floor;
         secondFloor = cubeBlocks.floors[1].floor;
 
         if (cube333)
             thirdFloor = cubeBlocks.floors[2].floor;
+        if (cube444)
+        {
+            thirdFloor = cubeBlocks.floors[2].floor;
+            fourthFloor = cubeBlocks.floors[3].floor;
+        }
     }
 
     public void Set333CubeAlpha()
@@ -50,6 +62,16 @@ public class CubeMaterial : MonoBehaviour
         SetFloorAlpha(firstFloor, new int[] { 1, 2, 3, 4, 6, 7, 8, 9 }, isAlpha ? 1.0f : alphaVal);
         SetFloorAlpha(secondFloor, new int[] { 1, 2, 3, 4, 6, 7, 8, 9 }, isAlpha ? 1.0f : alphaVal);
         SetFloorAlpha(thirdFloor, new int[] { 1, 2, 3, 4, 6, 7, 8, 9 }, isAlpha ? 1.0f : alphaVal);
+
+        isAlpha = !isAlpha;
+    }
+
+    public void Set444CubeAlpha()
+    {
+        SetFloorAlpha(firstFloor, new int[] { 1, 2, 3, 4, 5, 8, 9, 12, 13, 14, 15, 16 }, isAlpha ? 1.0f : alphaVal);
+        SetFloorAlpha(secondFloor, new int[] { 1, 2, 3, 4, 5, 8, 9, 12, 13, 14, 15, 16 }, isAlpha ? 1.0f : alphaVal);
+        SetFloorAlpha(thirdFloor, new int[] { 1, 2, 3, 4, 5, 8, 9, 12, 13, 14, 15, 16 }, isAlpha ? 1.0f : alphaVal);
+        SetFloorAlpha(fourthFloor, new int[] { 1, 2, 3, 4, 5, 8, 9, 12, 13, 14, 15, 16 }, isAlpha ? 1.0f : alphaVal);
 
         isAlpha = !isAlpha;
     }
