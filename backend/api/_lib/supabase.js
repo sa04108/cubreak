81144b0dace0cf7f27e75ed3dc50605aab7a6c64
전현배 <hyunbae204@daimresearch.com>
2025-08-22 17:42:47 +0900

    로컬 디버깅을 위해 로컬 환경변수 세팅

--- backend/api/_lib/supabase.js
+++ backend/api/_lib/supabase.js
@@ -1,8 +1,14 @@
+// dotenv로 로컬 환경 변수 가져오기
+// https://www.npmjs.com/package/dotenv
+// .env.local로 vercel 환경 변수 가져오기: vercel env pull .env.local
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env.local' })
+
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseClient = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 // CORS 헤더 (Unity WebGL용)
